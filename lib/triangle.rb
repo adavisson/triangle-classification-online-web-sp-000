673d7e4a05fe835aaea9a35f4b56f5fcde0675a2
Andrew Davisson <akdavisson4@gmail.com>
2019-06-14 20:26:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,6 +12,8 @@ class Triangle
   def kind
     if self.side_1 == self.side_2 && self.side_2 == self.side_3
       :equilateral
+    elsif self.side_1 == self.side_2 || self.side_1 == self.side_3 || self.side_2 == self.side_3
+      :isosceles
     end
   end
   
